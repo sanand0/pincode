9e58ba22f7d89ac719ebf74f6f562558681f8971
S Anand <root.node@gmail.com>
2011-03-06 17:35:59 +0000

    Download file created, and scheduled as a cron job

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import os, datetime
+import os, datetime, csv, StringIO
 from google.appengine.ext import webapp, db
 from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import login_required
-from google.appengine.api import users
+from google.appengine.api import users, taskqueue
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 from google.appengine.dist import use_library
@@ -27,6 +27,9 @@ class Event(db.Model):
     user    = db.UserProperty()
     time    = db.DateTimeProperty(auto_now_add=True)
     text    = db.StringProperty()
+
+class Download(db.Model):
+    text    = db.TextProperty()
 
 
 class HomePage(webapp.RequestHandler):
@@ -85,9 +88,38 @@ class CodePage(webapp.RequestHandler):
         Event(user=user,text=code).put()
         self.redirect('/')
 
+class BuildDownload(webapp.RequestHandler):
+    def get(self):
+        count    = int(self.request.get('count') or 0)
+        last_key = self.request.get('last')
+
+        q = PostalCode.all().order('__key__')
+        if last_key: q = q.filter('__key__ > ', db.Key(last_key))
+
+        text = StringIO.StringIO()
+        out = csv.writer(text)
+        codes = q.fetch(500)
+
+        if not codes: return
+
+        for p in codes:
+            country, postcode = p.key().name().split('/')
+            out.writerow([country, postcode, p.place_name, p.admin_name1,
+                p.latitude, p.longitude, p.accuracy])
+
+        Download(key_name = str(count), text=text.getvalue()).put()
+        taskqueue.add(url=self.request.path, method='GET', params={
+            'count': str(count+1),
+            'last': codes[-1].key(),
+        })
+        self.response.out.write('Download file #' + str(count) + ' built.')
+
+
 class DownloadPage(webapp.RequestHandler):
     def get(self):
-        self.response.out.write('Download will be available shortly. In the meantime, mail Anand: root.node@gmail.com')
+        self.response.headers['Content-Type'] = 'text/plain'
+        for data in Download.all().order('__key__'):
+            self.response.out.write(data.text)
 
 class LoginPage(webapp.RequestHandler):
     def get(self):
@@ -102,6 +134,7 @@ application = webapp.WSGIApplication([
     ('/',                   HomePage),
     ('/([A-Z][A-Z]/\d+)',   CodePage),
     ('/feed',               FeedPage),
+    ('/_download',          BuildDownload),
     ('/download',           DownloadPage),
     ('/login',              LoginPage),
     ('/logout',             LogoutPage),
